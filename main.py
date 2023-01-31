74eeeec70814557ef76b7bef2995bd0ab9aa5ad2
Jesus <navarro.jesusalfredo@gmail.com>
2023-01-31 00:42:18 -0600

    detect numbers

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-from time import sleep 
+import sys
 
-class Estate():
+D = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+class State():
   def __init__(self, id, rules):
     self.id = id
     self.rules = rules
@@ -15,26 +16,48 @@ class Estate():
         break
     return nextState
 
-def main():
-  E1 = Estate(1, {
-    2: "val == 1"
+def main(c):
+  E1 = State(1, {
+    2: "val in D"
   })
-  E2 = Estate(2, {
-    1: "val == 1"
+  E2 = State(2, {
+    2: "val in D",
+    3: "val == '.'"
+  })
+  E3 = State(3, {
+    4: "val in D"
+  })
+  E4 = State(4, {
+    4: "val in D"
   })
 
   initState = 1
   state = initState
-  while True:
-    val = random.randint(0, 5)
+  cadena = c
+  finalState = [4, 2]
+  for i in cadena:
+    val = i
     switch = {
       1: E1,
-      2: E2
+      2: E2,
+      3: E3,
+      4: E4
     }
     fun = switch.get(state)
     state = fun.netState(val)
-    sleep(1)
+    if state == -1:
+      indexError = i
+      break
   pass
+  print("Input " + cadena)
+  if state in finalState:
+    print("Correct")
+  else:
+    print("Incorrect in '" + indexError + "'")
 
 if __name__ == "__main__":
-  main()
+  try:
+    main(sys.argv[1])
+  except Exception as e:
+    print(e)
+    print("Send params\nEx.\n>python main.py 1000.1000")
